e2f52e98d78b200e9cbf6ddf811b1ea0c93ee5c9
Lex <leonardo.restivo@gmail.com>
2019-07-31 11:10:54 +0200

    pinmode init + builtin led

--- Looming_&_sound/arduino_TTL_OUT/arduino_TTL_OUT.ino
+++ Looming_&_sound/arduino_TTL_OUT/arduino_TTL_OUT.ino
@@ -3,7 +3,8 @@
 
 
  void setup() {
-//   pinMode(LED_BUILTIN, OUTPUT);
+   pinMode(LED_BUILTIN, OUTPUT);
+   pinMode(TTL,OUTPUT);
    Serial.begin(9600); // Start serial communication at 9600 bps
  }
 
@@ -15,9 +16,10 @@
    }
    if (val == '1') 
    { // If 1 was received
+         digitalWrite(LED_BUILTIN, HIGH);    // turn the LED off by making the voltage LOW
      digitalWrite(TTL, HIGH);   // turn the LED on (HIGH is the voltage level)
    } else {
-//     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+     digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
         digitalWrite(TTL, LOW);   // turn the LED on (HIGH is the voltage level)
    }
    delay(10); // Wait 10 milliseconds for next reading
